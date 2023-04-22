f3ad4a44a7c2029f6a08d92c1e6eac20a1af19be
CatDBB101 <catdbb1000@gmail.com>
2023-04-22 19:52:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -286,6 +286,14 @@ expressApp.get("/api/menu/:type/:id", (req, res) => {
     }
 });
 
+expressApp.options('/api/menu/:type/:id', function(req, res, next){
+   res.header('Access-Control-Allow-Origin', "*");
+   res.header('Access-Control-Allow-Methods', 'POST');
+   res.header("Access-Control-Allow-Headers", "accept, content-type");
+   res.header("Access-Control-Max-Age", "1728000");
+   return res.sendStatus(200);
+});
+
 // TODO : Port setting
 const port = 4000;
 expressApp.listen(port, () => {
