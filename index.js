c4f88c56c7fe7c4dabcfcf687aef3ba84238ce5b
CatDBB101 <catdbb1000@gmail.com>
2023-04-30 19:31:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -298,7 +298,8 @@ expressApp.post("/api/account/register", cors(), async (req, res) => {
                 password: password
             })
                 .then(result => {
-                    res.cookie("LoginKey", result._id, { maxAge: 1000 * 60 * 60 });
+                    res.setHeader('Set-Cookie', "LoginKey="+result._id);
+                    //res.cookie("LoginKey", result._id, { maxAge: 1000 * 60 * 60 });
                     res.send(["Created"]);
                 })
                 .catch(err => {
